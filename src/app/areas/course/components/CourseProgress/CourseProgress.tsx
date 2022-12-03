77a39d1bbed1ce072ed335735baa3c0f18dfec3d
FrameMuse <minicablestone@gmail.com>
2022-12-03 23:05:21 +0300

    add progress scale `CourseProgress`

--- src/app/areas/course/components/CourseProgress/CourseProgress.tsx
+++ src/app/areas/course/components/CourseProgress/CourseProgress.tsx
@@ -12,7 +12,7 @@ function CourseProgress(props: CourseProgressProps) {
   return (
     <div className="course-progress">
       <div className="course-progress__info">
-        <div className="course-progress-level">Level: <strong>{levelProgressMap(progress)}</strong></div>
+        <div className="course-progress-level">Level: <strong>{levelProgressSwitch(progress)}</strong></div>
         <div className="course-progress-completed"><strong>{props.completed}/{props.total}</strong> Lessons Completed</div>
       </div>
       <ProgressBar value={progress} />
@@ -20,12 +20,20 @@ function CourseProgress(props: CourseProgressProps) {
   )
 }
 
-function levelProgressMap(progress: number) {
-  if (progress >= 25) {
-    return "Middle Engineer"
-  }
+function levelProgressSwitch(progress: number) {
+  if (progress >= 0) return "Beginner"
+  if (progress >= 10) return "Novice"
+  if (progress >= 20) return "Apprentice"
+  if (progress >= 30) return "Advanced Beginner"
+  if (progress >= 40) return "Adept"
+  if (progress >= 50) return "Intermediate"
+  if (progress >= 60) return "Veteran"
+  if (progress >= 70) return "Professional"
+  if (progress >= 80) return "Expert"
+  if (progress >= 90) return "Elite"
+  if (progress >= 100) return "Algo Master"
 
-  return "Junior Engineer"
+  return "Beginner"
 }
 
 export default CourseProgress
