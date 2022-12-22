ffec456b47e060b3f5965388cdf2c09d018d671d
FrameMuse <minicablestone@gmail.com>
2022-12-23 00:05:52 +0300

    remove toast

--- src/api/hooks/lessons/useUpdateLessonNotes.ts
+++ src/api/hooks/lessons/useUpdateLessonNotes.ts
@@ -1,12 +1,11 @@
 import appQuery from "api/appQuery"
 import { APIActions } from "api/data"
-import { toast } from "react-toastify"
 
 function useUpdateLessonNotes() {
   async function updateLessonNotes(id: string, content: string) {
     await appQuery(APIActions.patchNotes(id, { content }))
 
-    toast.success("Notes has beed updated.")
+    // toast.success("Notes has beed updated.")
   }
 
   return updateLessonNotes
