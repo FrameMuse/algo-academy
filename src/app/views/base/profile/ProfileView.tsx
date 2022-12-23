93144e8a63fdb3aca8a90dd96bc054defd37d473
FrameMuse <minicablestone@gmail.com>
2022-12-23 23:27:53 +0300

    add logout button

--- src/app/views/base/profile/ProfileView.tsx
+++ src/app/views/base/profile/ProfileView.tsx
@@ -2,6 +2,7 @@ import "./ProfileView.scss"
 
 import useUserResetData from "api/hooks/user/useUserResetData"
 import { APP_TITLE } from "app/App"
+import { StaticRoutes } from "app/AppRoutes"
 import { UserGeneralInfo, UserHasNoPlanNotice, UserProblemsSolved, UserPurchaseHistory } from "app/areas/user"
 import Box from "app/layouts/Box/Box"
 import Headings from "app/layouts/Headings/Headings"
@@ -9,8 +10,11 @@ import { confirmAction } from "app/popups/PopupConfirm/PopupConfirm"
 import Button from "app/ui/kit/Button/Button"
 import AppNavLink from "app/ui/kit/Link/AppNavLink"
 import { Helmet } from "react-helmet"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
+import { useLocalStorage } from "react-use"
 import { GAEventLabel } from "services/ga"
+import { useAppDispatch } from "store/hooks"
+import { updateUser, USER_GUEST } from "store/reducers/user"
 
 enum ProfileViewRoutes {
   MyAccount = "", // relative root
@@ -19,6 +23,8 @@ enum ProfileViewRoutes {
 }
 
 function ProfileView() {
+  const logout = useUserLogout()
+
   return (
     <section className="page-section account-section">
       <Helmet>
@@ -35,6 +41,7 @@ function ProfileView() {
               <AppNavLink to={ProfileViewRoutes.MyAccount} className="account-menu-link" end>My Account</AppNavLink>
               <AppNavLink to={ProfileViewRoutes.MyPurcase} className="account-menu-link">My Purchase</AppNavLink>
               <AppNavLink to={ProfileViewRoutes.MyProgress} className="account-menu-link">Account Data</AppNavLink>
+              <Button color="dark" className="account-menu-link" onClick={logout}>Log out</Button>
             </div>
           </div>
           <div className="account-right">
@@ -48,6 +55,24 @@ function ProfileView() {
       </div>
     </section>
   )
+}
+
+
+/**
+ * TODO: Better move from here.
+ */
+function useUserLogout() {
+  const dispatch = useAppDispatch()
+  const [, setUserToken] = useLocalStorage("user-token")
+  const navigate = useNavigate()
+
+  function logout() {
+    dispatch(updateUser(USER_GUEST))
+    setUserToken(null)
+    navigate(StaticRoutes.Home)
+  }
+
+  return logout
 }
 
 function AccountView() {
