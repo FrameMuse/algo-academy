9ae7fd98abaa73d9ea4e8ec73b54aa08553bf57c
FrameMuse <minicablestone@gmail.com>
2023-01-05 19:01:16 +0300

    fix smth

--- src/app/areas/lesson/components/CodeSubmition/CodeSubmition.tsx
+++ src/app/areas/lesson/components/CodeSubmition/CodeSubmition.tsx
@@ -109,7 +109,16 @@ function CodeSubmitionResult(props: CodeSubmitionResultProps) {
       <List>
         {props.result.tests.map((test, index) => (
           <ListItem icon={test.passed ? "check" : "cross"} key={index}>
-            <strong>{test.description}</strong> - Got {JSON.stringify(test.userAnswer)} instead of {JSON.stringify(test.expected)}.
+            {test.passed && (
+              <>
+                <strong>{test.description}</strong> - Passed.
+              </>
+            )}
+            {!test.passed && (
+              <>
+                <strong>{test.description}</strong> - Got {JSON.stringify(test.userAnswer)} instead of {JSON.stringify(test.expected)}.
+              </>
+            )}
           </ListItem>
         ))}
       </List>
