da2dab99ca86dd3c50eded73eb820113c5b8fecf
FrameMuse <minicablestone@gmail.com>
2023-01-28 00:53:54 +0300

    Revert "privacy policy update"
    This reverts commit 67949ee52c5c5e5ff3ad1cbcd9e4183a9f764153.

--- src/app/views/base/privacy-policy/PrivacyPolicyView.tsx
+++ src/app/views/base/privacy-policy/PrivacyPolicyView.tsx
@@ -6,48 +6,86 @@ function PrivacyPolicyView() {
   return (
     <div className="wrapper">
       <Article className="privacy-policy-view">
-        <h1>Terms and Conditions for Algo Academy, LLC</h1>
-        <h2>Introduction</h2>
-        <p>These Website Standard Terms and Conditions written on this webpage shall manage your use of our website, Algo Academy accessible at algoacademy.io.</p>
-        <p>These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed to accept all terms and conditions written in here. You must not use this Website if you disagree with any of these Website Standard Terms and Conditions. These Terms and Conditions have been generated with the help of the <a href="https://www.termsandcondiitionssample.com">Terms And Conditiions Sample Generator</a>.</p>
-        <p>Minors or people below 18 years old are not allowed to use this Website.</p>
-        <h2>Intellectual Property Rights</h2>
-        <p>Other than the content you own, under these Terms, Algo Academy, LLC and/or its licensors own all the intellectual property rights and materials contained in this Website.</p>
-        <p>You are granted limited license only for purposes of viewing the material contained on this Website.</p>
-        <h2>Restrictions</h2>
-        <p>You are specifically restricted from all of the following:</p>
+        <h1>Privacy Policy for , LLC</h1>
+
+        <p>At Algo Academy, accessible from algoacademy.io, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Algo Academy and how we use it.</p>
+
+        <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
+
+        <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Algo Academy. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the <a href="https://www.privacypolicygenerator.info/">Free Privacy Policy Generator</a>.</p>
+
+        <h2>Consent</h2>
+
+        <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
+
+        <h2>Information we collect</h2>
+
+        <p>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</p>
+        <p>If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
+        <p>When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</p>
+
+        <h2>How we use your information</h2>
+
+        <p>We use the information we collect in various ways, including to:</p>
+
         <ul>
-          <li>publishing any Website material in any other media;</li>
-          <li>selling, sublicensing and/or otherwise commercializing any Website material;</li>
-          <li>publicly performing and/or showing any Website material;</li>
-          <li>using this Website in any way that is or may be damaging to this Website;</li>
-          <li>using this Website in any way that impacts user access to this Website;</li>
-          <li>using this Website contrary to applicable laws and regulations, or in any way may cause harm to the Website, or to any person or business entity;</li>
-          <li>engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this Website;</li>
-          <li>using this Website to engage in any advertising or marketing.</li>
+          <li>Provide, operate, and maintain our website</li>
+          <li>Improve, personalize, and expand our website</li>
+          <li>Understand and analyze how you use our website</li>
+          <li>Develop new products, services, features, and functionality</li>
+          <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
+          <li>Send you emails</li>
+          <li>Find and prevent fraud</li>
         </ul>
-        <p>Certain areas of this Website are restricted from being access by you and Algo Academy, LLC may further restrict access by you to any areas of this Website, at any time, in absolute discretion. Any user ID and password you may have for this Website are confidential and you must maintain confidentiality as well.</p>
-        <h2>Your Content</h2>
-        <p>In these Website Standard Terms and Conditions, {`"Your Content"`} shall mean any audio, video text, images or other material you choose to display on this Website. By displaying Your Content, you grant Algo Academy, LLC a non-exclusive, worldwide irrevocable, sub licensable license to use, reproduce, adapt, publish, translate and distribute it in any and all media.</p>
-        <p>Your Content must be your own and must not be invading any {"third-party’s"} rights. Algo Academy, LLC reserves the right to remove any of Your Content from this Website at any time without notice.</p>
-        <h2>Your Privacy</h2>
-        <p>Please read Privacy Policy.</p>
-        <h2>No warranties</h2>
-        <p>This Website is provided {`"as is,"`} with all faults, and Algo Academy, LLC express no representations or warranties, of any kind related to this Website or the materials contained on this Website. Also, nothing contained on this Website shall be interpreted as advising you.</p>
-        <h2>Limitation of liability</h2>
-        <p>In no event shall Algo Academy, LLC, nor any of its officers, directors and employees, shall be held liable for anything arising out of or in any way connected with your use of this Website whether such liability is under contract. &nbsp;Algo Academy, LLC, including its officers, directors and employees shall not be held liable for any indirect, consequential or special liability arising out of or in any way related to your use of this Website.</p>
-        <h2>Indemnification</h2>
-        <p>You hereby indemnify to the fullest extent Algo Academy, LLC from and against any and/or all liabilities, costs, demands, causes of action, damages and expenses arising in any way related to your breach of any of the provisions of these Terms.</p>
-        <h2>Severability</h2>
-        <p>If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.</p>
-        <h2>Variation of Terms</h2>
-        <p>Algo Academy, LLC is permitted to revise these Terms at any time as it sees fit, and by using this Website you are expected to review these Terms on a regular basis.</p>
-        <h2>Assignment</h2>
-        <p>The Algo Academy, LLC is allowed to assign, transfer, and subcontract its rights and/or obligations under these Terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your rights and/or obligations under these Terms.</p>
-        <h2>Entire Agreement</h2>
-        <p>These Terms constitute the entire agreement between Algo Academy, LLC and you in relation to your use of this Website, and supersede all prior agreements and understandings.</p>
-        <h2>Governing Law &amp; Jurisdiction</h2>
-        <p>These Terms will be governed by and interpreted in accordance with the laws of the State of us, and you submit to the non-exclusive jurisdiction of the state and federal courts located in us for the resolution of any disputes.</p>
+
+        <h2>Log Files</h2>
+
+        <p>Algo Academy follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting {"services'"} analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking {"users'"} movement on the website, and gathering demographic information.</p>
+
+        <h2>Cookies and Web Beacons</h2>
+
+        <p>Like any other website, Algo Academy uses {"'cookies'"}. These cookies are used to store information including {"visitors'"} preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the {"users'"} experience by customizing our web page content based on {"visitors'"} browser type and/or other information.</p>
+
+
+
+        <h2>Advertising Partners Privacy Policies</h2>
+
+        <p>You may consult this list to find the Privacy Policy for each of the advertising partners of Algo Academy.</p>
+
+        <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Algo Academy, which are sent directly to {"users'"} browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+
+        <p>Note that Algo Academy has no access to or control over these cookies that are used by third-party advertisers.</p>
+
+        <h2>Third Party Privacy Policies</h2>
+
+        <p>Algo {"Academy's"} Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
+
+        <p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the {"browsers'"} respective websites.</p>
+
+        <h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
+
+        <p>Under the CCPA, among other rights, California consumers have the right to:</p>
+        <p>Request that a business that collects a {"consumer's"} personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</p>
+        <p>Request that a business delete any personal data about the consumer that a business has collected.</p>
+        <p>Request that a business that sells a {"consumer's"} personal data, not sell the {"consumer's"} personal data.</p>
+        <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
+
+        <h2>GDPR Data Protection Rights</h2>
+
+        <p>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
+        <p>The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.</p>
+        <p>The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.</p>
+        <p>The right to erasure – You have the right to request that we erase your personal data, under certain conditions.</p>
+        <p>The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.</p>
+        <p>The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.</p>
+        <p>The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</p>
+        <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
+
+        <h2>{"Children's"} Information</h2>
+
+        <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
+
+        <p>Algo Academy does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
       </Article>
     </div>
   )
